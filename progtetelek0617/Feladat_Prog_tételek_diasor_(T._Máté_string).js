df94fcf07c57a5296f4f839b198fb1413470a5fb
Gabor Veres <veres.gabor.attila@freemail.hu>
2019-06-19 10:06:59 +0200

    new addition

--- progtetelek0617/Feladat_Prog_tételek_diasor_(T._Máté_string).js
+++ progtetelek0617/Feladat_Prog_tételek_diasor_(T._Máté_string).js
@@ -1,3 +1,29 @@
-const palindom = (str) => {
-    if () 
+const palindrom = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (str === /[^A-Za-z0–9]/g) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+console.log(palindrom('indul a görög aludni'));
+
+function palindrome (str) {
+  str = str.toLowerCase();
+  str = str.replace(',', '');
+  str = str.replace('.', '');
+  str = str.replace(':', '');
+  str = str.replace(';', '');
+  str = str.replace('-', '');
+  str = str.replace(',', '');
+  str = str.replace(' ', '');
+  for (var i = 0; i <= (str.length / 2); i++) {
+    if (str[i] != str.length - i) return false;
+  }
+  return true;
 }
+
+let str = 'indul a görög aludni';
+console.log(palindrome(str));
